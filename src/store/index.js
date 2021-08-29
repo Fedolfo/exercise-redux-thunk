95a5e8a21c3fa6de8fdc9402a295346c156fcf5b
Filipe <filipecandido123@hotmail.com>
2021-08-29 18:59:01 -0300

    async

--- src/store/index.js
+++ src/store/index.js
@@ -1,11 +1,9 @@
 // Importe o método applyMiddleware
-import { createStore } from 'redux';
-// Importe o redux-thunk
+import { createStore, applyMiddleware } from 'redux';// Importe o redux-thunk
 import rootReducer from '../reducers';
+import thunk from 'redux-thunk';
 
 // Aplique o middleware
-const store = createStore(
-  rootReducer,
-);
+const store = createStore(rootReducer, applyMiddleware(thunk));
 
 export default store;
